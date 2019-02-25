5130a5d128e1259f21ac61899969552926eea7f8
Jackie <JGtheophila19@gmail.com>
2019-02-25 11:41:49 -0500

    loop and push empty subarrays into bookedRooms

--- multiArray.js
+++ multiArray.js
@@ -4,10 +4,14 @@ var hotelName = "Javascript Function Hotel";
 var hotelMotto = "What's your function";
 
 var roomTypes = ['twin', 'single', 'double', 'queen', 'king'];
-var roomNumbers = [[101, 102, 103, 104], [105, 106, 107], [201, 202], [203, 204], [301]];
+var roomNumbers = [[101, 102, 103, 104], [105, 106, 107, 108], [201, 202], [203, 204], [301, 302, 303], [304, 305]];
 var roomPrices = [75, 85, 100, 150, 200];
-
 var bookedRooms = [[], [], [], [], []];
+
+// loop and push empty subarrays into bookedRooms for as many elements that exists in roomTypes
+for(var i = 0; i < roomTypes.lenght; i++){
+    bookedRooms.push([]);
+}
 
 function availableRoomQty(){
     var totalRoomQty = 0;
@@ -23,12 +27,15 @@ function availableRoomsReport(){
 
 function bookTwin(){
     bookedRooms[0].sort(bookedRooms[0].push(roomNumbers[0].pop()));
+    // looks at the first array in bookedRooms sorting the rooms in the first subarray 
+    // and pops out the last number in the array which is roomNumber 104
 }
 
 function bookSingle(){
     bookedRooms[1].push(roomNumbers[1].pop());
 }
 
+// 1. calculate bookedRoom[0].length * roomPrices
 function salesReport(){
     var totalSales = 0;
     for(var i = 0; i < bookedRooms.length; i++){
