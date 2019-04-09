6ea586bc3f83be66a9202194254c41f860f86751
Jackie <JGtheophila19@gmail.com>
2019-04-09 19:55:56 -0400

    creating a variable

---
+++ variable.js
@@ -0,0 +1,32 @@
+var myName = 'Arya';
+// var is short for variable, is the JavaScript keyword that will create a new variable for us.
+
+/* myName is chosen by a developer. The word has no spaces, and each new word is capitalized. 
+This is a common convention in JavaScript, and is called camelCase. */
+
+// = means to assign whatever’s next to the variable.
+
+// 'Arya' is the value that the equals = assigns into the variable myName.
+
+console.log(myName);
+// Output: Arya
+
+/* After the variable is declared, we can print the variable with: console.log(myName). 
+This will print 'Arya' to the console.*/
+
+
+/* Variables can hold any data type, like strings, numbers, and Booleans. 
+They can also hold data types that we have not learned yet, like arrays, functions and objects */
+
+
+var age = 27;
+console.log(age)
+// output: 27
+
+var hasPet = true;
+console.log(hasPet);
+// output: true
+
+var strength = '50,0000';
+console.log("How much stuff can a variable hold?", strength);
+// output: How much stuff can a variable hold? 50,0000
